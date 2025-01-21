97031d6e6cfe2bc697ffac8fd70b5e5220b5fb4b
Johnson <23009886@myrp.edu.sg>
2025-01-21 15:47:42 +0800

    cors import

--- backend/server.js
+++ backend/server.js
@@ -1,24 +1,21 @@
 import 'dotenv/config'; // Load environment variables
 import express from 'express';
 import mongoose from 'mongoose';
+import cors from 'cors'; // Use import for CORS since it's an ES module
 import workoutRoutes from './routes/workouts.js'; // Ensure to include the .js extension
-
-//Require cors
-const cors = require("cors");
 
 // Express app
 const app = express();
 
-
 // Middleware
-app.use(express.json());
+app.use(express.json()); // Parse JSON body
+app.use(cors()); // Enable CORS for all routes
 
+// Log requests
 app.use((req, res, next) => {
   console.log(req.path, req.method);
   next();
 });
-
-app.use(cors());
 
 // Routes
 app.use('/api/workouts', workoutRoutes);
@@ -27,11 +24,11 @@ app.use('/api/workouts', workoutRoutes);
 mongoose.connect(process.env.MONGO_URI)
   .then(() => {
     console.log('Connected to database');
-    // Listen on port
+    // Listen on the specified port
     app.listen(process.env.PORT, () => {
       console.log('Listening for requests on port', process.env.PORT);
     });
   })
   .catch((err) => {
-    console.error(err);
+    console.error('Database connection error:', err);
   });
